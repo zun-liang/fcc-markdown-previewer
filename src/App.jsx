7eca0ee78bb9cc0be642b5005c1f3800d3647a56
Zun Liang <107293493+zun-liang@users.noreply.github.com>
2023-11-10 18:26:20 +0800

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Editor from "./components/Editor";
+import Previewer from "./components/Previewer";
+
+const App = () => {
+  return (
+    <>
+      <Editor />
+      <Previewer />
+    </>
+  );
+};
+
+export default App;
---
+++ src/components/Previewer.jsx
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+const Previewer = () => {
+  return <></>;
+};
+
+export default Previewer;
