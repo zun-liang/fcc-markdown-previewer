0dee05bf8c438bb40874b661a114e7c5f97f2de2
Zun Liang <107293493+zun-liang@users.noreply.github.com>
2023-11-14 02:21:38 +0800

    set initial text

--- src/markdown.js
+++ src/markdown.js
@@ -1,2 +1,43 @@
-export const initialText =
-  "# Welcome to my React Markdown Previewer!  \n\n ## This is a sub-heading... \n\n ### And here's some other cool stuff:  \n\n Heres some code, `<div></div>`, between 2 backticks.  \n\n ``` // this is multi-line code:  \n\n function anotherExample(firstLine, lastLine) {   \n\n if (firstLine == '```' && lastLine == '```') {     \n\n return multiLineCode;   \n\n } \n\n } \n\n ```  \n\n You can also make text **bold**... whoa! Or _italic_. Or... wait for it... **_both!_** \n\n And feel free to go crazy ~~crossing stuff out~~.  \n\n There's also [links](https://www.freecodecamp.org), and > Block Quotes!  \n\n And if you want to get really crazy, even tables:  \n\n Wild Header | Crazy Header | Another Header? \n\n ------------ | ------------- | ------------- \n\n Your content can | be here, and it | can be here.... \n\n And here. | Okay. | I think we get it.  \n\n - And of course there are lists.   \n\n - Some are bulleted.      \n\n - With different indentation levels.         \n\n - That look like this.   \n\n &Tab;1. And there are numbered lists too. \n\n &Tab;1. Use just 1s if you want! \n\n &Tab;1. And last but not least, let's not forget embedded images:  \n\n ![freeCodeCamp Logo](https://cdn.freecodecamp.org/testable-projects-fcc/images/fcc_secondary.svg)";
+export const initialText = `# Welcome to my React Markdown Previewer!
+
+## This is a sub-heading...
+### And here's some other cool stuff:
+
+Heres some code, \`<div></div>\`, between 2 backticks.
+
+\`\`\`
+// this is multi-line code:
+
+function anotherExample(firstLine, lastLine) {
+  if (firstLine == '\`\`\`' && lastLine == '\`\`\`') {
+    return multiLineCode;
+  }
+}
+\`\`\`
+
+You can also make text **bold**... whoa!
+Or _italic_.
+Or... wait for it... **_both!_**
+And feel free to go crazy ~~crossing stuff out~~.
+
+There's also [links](https://www.freecodecamp.org), and
+> Block Quotes!
+
+And if you want to get really crazy, even tables:
+
+Wild Header | Crazy Header | Another Header?
+------------ | ------------- | -------------
+Your content can | be here, and it | can be here....
+And here. | Okay. | I think we get it.
+
+- And of course, there are lists.
+  - Some are bulleted.
+     - With different indentation levels.
+        - That look like this.
+
+1. And there are numbered lists too.
+1. Use just 1s if you want!
+1. And last but not least, let's not forget embedded images:
+
+![freeCodeCamp Logo](https://cdn.freecodecamp.org/testable-projects-fcc/images/fcc_secondary.svg)
+`;
