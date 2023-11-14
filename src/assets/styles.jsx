4bb88436a21695dd361fd02f9ee127c1cf5324cf
Zun Liang <107293493+zun-liang@users.noreply.github.com>
2023-11-14 23:27:37 +0800

    update styles

--- src/assets/styles.jsx
+++ src/assets/styles.jsx
@@ -1,16 +1,16 @@
 import styled from "styled-components";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-export const SharedBox = styled.div`
-  box-shadow: 1px 1px 10px 2px #3a5f5f;
-`;
+export const SharedBox = styled.div``;
 export const SharedToolbar = styled.div`
   width: 100%;
   height: 2rem;
-  background-color: #4aa3a3;
+  background-color: var(--green);
   padding: 5px 10px;
-  border: 1px solid #1d2f2f;
-  box-shadow: 1px 1px 10px 2px #3a5f5f;
+  border-top: 1px solid var(--very-dark-green);
+  border-left: 1px solid var(--very-dark-green);
+  border-right: 1px solid var(--very-dark-green);
+  box-shadow: 1px 1px 5px 2px var(--dark-green);
   display: grid;
   align-items: center;
   grid-template-columns: 1rem 1fr 1rem;
